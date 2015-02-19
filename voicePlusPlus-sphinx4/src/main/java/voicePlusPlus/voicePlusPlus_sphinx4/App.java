bdd12c6c43f74f16d1e19b4d325753ef002c2197
chrisatanasian <catanasian@gmail.com>
2015-02-19 02:32:50 -0800

    Basics of transcribing from a file now works. Only works for very clear audio files

--- voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/App.java
+++ voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/App.java
@@ -1,6 +1,14 @@
 package voicePlusPlus.voicePlusPlus_sphinx4;
 
 import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.result.WordResult;
+
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 
 /**
  * Hello world!
@@ -10,7 +18,34 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Configuration config;
-        System.out.println( "Hello World!" );
+    	Configuration config = new Configuration();
+    	config.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+    	config.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+    	config.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.dmp");
+    	
+        try {
+    		StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(config);
+    	    InputStream stream = new FileInputStream("../sphinx4-core/target/test-classes/edu/cmu/sphinx/tools/bandwidth/10001-90210-01803.wav");
+
+    	    recognizer.startRecognition(stream);
+            SpeechResult result;
+            while ((result = recognizer.getResult()) != null) {
+                System.out.format("Hypothesis: %s\n", result.getHypothesis());
+
+                System.out.println("List of recognized words and their times:");
+                for (WordResult r : result.getWords()) {
+                    System.out.println(r);
+                }
+
+                System.out.println("Best 3 hypothesis:");
+                for (String s : result.getNbest(3))
+                    System.out.println(s);
+
+            }
+            recognizer.stopRecognition();
+    	} catch (IOException e) {
+    		System.err.println(e.getMessage());
+    		e.printStackTrace();
+    	}
     }
 }
