5b3a4190daa25fa1f3f316e854b8f4ded9b26e0b
Chris Atanasian <catanasian@gmail.com>
2015-02-26 22:18:19 -0800

    Hashtable of keywords instantiable from file

--- voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/Keyword.java
+++ voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/Keyword.java
@@ -2,13 +2,44 @@ package voicePlusPlus.voicePlusPlus_sphinx4;
 
 import java.util.Hashtable;
 import java.util.Set;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-	
 public class Keyword {
 	private static Hashtable<String, String> keywords;
 	
 	public static void instantiateHashTable(String fileName) {
 		keywords = new Hashtable<String, String>();
+		BufferedReader br = null;
+		String line;
+		String keywordCategory;
+		Boolean isKeyword;
+		
+		
+		try {			
+			br = new BufferedReader(new FileReader(fileName));
+			isKeyword = false;
+			keywordCategory = "";
+			
+			while ((line = br.readLine()) != null) {
+				if (line.equals("")) {
+					isKeyword = false;
+					continue;
+				}
+				
+				if (isKeyword) {
+					keywords.put(line, keywordCategory);
+				}
+				else {
+					keywordCategory = line;
+					isKeyword = true;
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
