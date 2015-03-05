f85cb61c156237792957f66df4e3795b6bf8b0f3
Belsin Barkhosir <belsin@umail.ucsb.edu>
2015-03-05 13:47:32 -0800

    updated APP.java to run test

--- voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/App.java
+++ voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/App.java
@@ -7,14 +7,22 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SphinxManager sphinxManager = new SphinxManager();
-		TaskManager taskManager = new TaskManager();
+		//TaskManager taskManager = new TaskManager();
 		sphinxManager.StartRecognizingAudio();
 		SpeechResult result;
+		
+		//RECOGNIZING SPEECH
 		while ((result = sphinxManager.GetSpeechResult()) != null)
 		{
 			String utterance = sphinxManager.GetUtterance(result);
 			System.out.println(utterance);
 		}
+		
+		GoogleCalendarAPICommand command = new GoogleCalendarAPICommand();
+		command.EventDescription = "schedule meeting tomorrow nine";
+		command.API = TaskManager.determineAPI(command.EventDescription);
+		
+		TaskManager.InvokeAPICommand(command);
 		sphinxManager.StopRecognizingAudio();
 	}
 
