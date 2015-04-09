9c233b847038f23fa91608c8c7ab8975476fc97c
Belsin Barkhosir <belsin@umail.ucsb.edu>
2015-04-08 22:51:31 -0700

    added app.java to Outbound Project to start making main loop that sets up the outbound stuff

---
+++ Outbound/src/main/java/voicePlusPlus/Outbound/App.java
@@ -0,0 +1,14 @@
+package voicePlusPlus.Outbound;
+
+import java.util.HashSet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+	}
+}
