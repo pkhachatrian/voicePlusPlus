5954efa8c859d46d78ed32d283a476a98b89036c
Belsin Barkhosir <belsin@umail.ucsb.edu>
2015-04-21 03:40:21 -0700

    quick updates

--- Outbound/src/main/java/voicePlusPlus/Outbound/AbstractOutboundPipelineFactory.java
+++ Outbound/src/main/java/voicePlusPlus/Outbound/AbstractOutboundPipelineFactory.java
@@ -15,56 +15,38 @@
  */
 package voicePlusPlus.Outbound;
 
-import org.freeswitch.esl.client.internal.AbstractEslClientHandler;
-import org.freeswitch.esl.client.transport.event.EslEvent;
-import org.freeswitch.esl.client.transport.message.EslMessage;
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.handler.execution.ExecutionHandler;
+import org.freeswitch.esl.client.internal.debug.ExecutionHandler;
+import org.freeswitch.esl.client.outbound.AbstractOutboundClientHandler;
+import org.freeswitch.esl.client.transport.message.EslFrameDecoder;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.handler.codec.string.StringEncoder;
+import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 
 /**
- * Specialised {@link AbstractEslClientHandler} that implements the base connecction logic for an 
- * 'Outbound' FreeSWITCH Event Socket connection.  The responsibilities for this class are:
- * <ul><li>
- * To send a 'connect' command when the FreeSWITCH server first establishes a new connection with
- * the socket client in Outbound mode.  This will result in an incoming {@link EslMessage} that is
- * transformed into an {@link EslEvent} that sub classes can handle.
- * </ul>
- * Note: implementation requirement is that an {@link ExecutionHandler} is placed in the processing 
- * pipeline prior to this handler. This will ensure that each incoming message is processed in its
- * own thread (although still guaranteed to be processed in the order of receipt).
+ * An abstract factory to assemble a Netty processing pipeline for outbound clients.
  * 
  * @author  david varnes
  */
-public abstract class AbstractOutboundClientHandler extends AbstractEslClientHandler
+public abstract class AbstractOutboundPipelineFactory implements ChannelPipelineFactory
 {
-
-    @Override
-    public void channelConnected( ChannelHandlerContext ctx, ChannelStateEvent e ) throws Exception
+    public ChannelPipeline getPipeline() throws Exception
     {
-        // Have received a connection from FreeSWITCH server, send connect response
-        log.debug( "Received new connection from server, sending connect message" );
+        ChannelPipeline pipeline = Channels.pipeline(); 
+        // Add the text line codec combination first
+        pipeline.addLast( "encoder", new StringEncoder() );
+        // Note that outbound mode requires the decoder to treat many 'headers' as body lines
+        pipeline.addLast( "decoder", new EslFrameDecoder( 8092, true ) );
+        // Add an executor to ensure separate thread for each upstream message from here
+        pipeline.addLast( "executor", new ExecutionHandler( 
+            new OrderedMemoryAwareThreadPoolExecutor( 16, 1048576, 1048576 ) ) );
+
+        // now the outbound client logic
+        pipeline.addLast( "clientHandler", makeHandler() );
         
-        EslMessage response = sendSyncSingleLineCommand( ctx.getChannel(), "connect" );
-        // The message decoder for outbound, treats most of this incoming message as an 'event' in 
-        // message body, so it parse now
-        EslEvent channelDataEvent = new EslEvent( response, true );
-        // Let implementing sub classes choose what to do next
-        handleConnectResponse( ctx, channelDataEvent );
+        return pipeline;
     }
 
-    protected abstract void handleConnectResponse( ChannelHandlerContext ctx, EslEvent event );
-
-    @Override
-    protected void handleAuthRequest( ChannelHandlerContext ctx )
-    {
-        // This should not happen in outbound mode
-        log.warn( "Auth request received in outbound mode, ignoring" ); 
-    }
-
-    @Override
-    protected void handleDisconnectionNotice()
-    {
-        log.debug( "Received disconnection notice" );
-    }    
+    protected abstract AbstractOutboundClientHandler makeHandler();
 }
--- Outbound/src/main/java/voicePlusPlus/Outbound/App.java
+++ Outbound/src/main/java/voicePlusPlus/Outbound/App.java
@@ -11,6 +11,7 @@ public class App {
 		
 		sc.start();
 		System.out.println("Starting server.");
+		
 //		sc.stop();
 	}
 }
