76a3bfc77b0227358a74d8032870c65d77ddb3c8
Belsin Barkhosir <belsin@umail.ucsb.edu>
2015-06-02 14:41:52 -0700

    quick update

--- voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/TaskManager.java
+++ voicePlusPlus-sphinx4/src/main/java/voicePlusPlus/voicePlusPlus_sphinx4/TaskManager.java
@@ -72,7 +72,9 @@ public class TaskManager {
 	 * @return the API as a string
 	 */
 	public static String determineAPI(String command) {
-		if ((command.contains("scratch that") || command.contains("delete") || command.contains("remove")) && (command.contains("bob") || command.contains("henry"))) 
+		if(command.contains("bob") || command.contains("henry"))
+			return APIs.GOOGLE_CALENDAR;
+		if (command.contains("scratch that") || command.contains("delete") || command.contains("remove")) 
 			return APIs.DELETE;
 		if (command.contains("list"))
 			return APIs.GOOGLE_CALENDAR_LIST;
